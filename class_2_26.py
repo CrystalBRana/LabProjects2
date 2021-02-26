d19ebd4d8f2415e2de149eb1db4c8811b6e6bde2
CrystalBRana <crystalkoinch@gmail.com>
2021-02-26 21:51:09 +0545

    debug

---
+++ class_2_26.py
@@ -0,0 +1,9 @@
+#  write a program to find a number is negative, positive or zero that is entered bye a user.
+a = int(input('Enter the number:'))
+if a<0:
+    print("It is negative number.")
+elif a>0:
+    print("It is positive number.")
+else:
+    print("It is zero.")
+print("The program is zero.")
---
+++ debug.py
@@ -0,0 +1,4 @@
+# write a program to print a number from 1 to 10.
+for i in range(2,51,2):
+    if i%2==0:
+        print('It is even.',i)
