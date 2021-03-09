5a121f059f82e21e22fca3774ff62dfa100bc36f
CrystalBRana <crystalkoinch@gmail.com>
2021-03-09 11:44:56 +0545

    Question number 8 area of circle

---
+++ q_n0_ 8.py
@@ -0,0 +1,5 @@
+''' Write a Python program  which accepts the radius of a circle  from the user  and compute the area.
+(area of circle =PI * r^2)'''
+radius = float(input("Enter the radius of circle in centimeter:"))
+area_of_circle = (3.14 * (radius**2))
+print(f"The area of circle is {area_of_circle} square meter")
