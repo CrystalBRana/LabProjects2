a4f56288695b675c3b0bff4a25667fb40989663a
CrystalBRana <crystalkoinch@gmail.com>
2021-03-16 09:46:03 +0545

    Question number 10

---
+++ q.no.10.py
@@ -0,0 +1,11 @@
+# Write a python program to convert seconds to day, hour, minutes and seconds.
+
+seconds = int(input('Insert second:'))
+seconds_in_day = 60*60*24
+seconds_in_hour = 60*60
+seconds_in_minute = 60
+
+days = seconds // seconds_in_day
+hours = (seconds - (days * seconds_in_day))// seconds_in_hour
+minutes = (seconds - (days * seconds_in_day) - (hours * seconds_in_hour)) // seconds_in_minute
+print(days)
