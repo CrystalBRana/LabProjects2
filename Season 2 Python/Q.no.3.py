5393f545a410c3dfb63e5c053df875021824e329
CrystalBRana <crystalkoinch@gmail.com>
2021-04-13 09:27:45 +0545

    Question No.3 Season 2

---
+++ Season 2 Python/Q.no.3.py
@@ -0,0 +1,8 @@
+# Check whether the user input number is even or odd and display it to user.
+
+num = int(input("Enter your number:"))
+if num % 2 == 0:
+    print("It is even number.")
+else:
+    print("It is odd number")
+print("Program is over:")
